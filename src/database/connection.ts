bc038c2367660b3fa6887439f4f36e36fa192b1e
Vinicius Mendite <vinicius.miff@gmail.com>
2020-06-03 14:33:11 -0300

    tabelas criadas

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,10 +2,11 @@ import knex from 'knex';
 import path from 'path';
 
 const connection = knex({
-  client: 'sqite3',
+  client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite'),
   },
+  useNullAsDefault: true,
 });
 
 export default connection;
